aae073a1ae7caf4483bcb13dc5e32f6d62c8eebe
Sergey <sergeybtw17@gmail.com>
2023-02-08 14:29:28 +0300

    add WillPopScope to editCinema route

--- lib/routes/edit_cinema.dart
+++ lib/routes/edit_cinema.dart
@@ -10,84 +10,94 @@ class EditCinema extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    RoutesData routesData = ModalRoute.of(context)?.settings.arguments as RoutesData;
+    RoutesData routesData =
+        ModalRoute.of(context)?.settings.arguments as RoutesData;
     //Cinema cinema = ModalRoute.of(context)?.settings.arguments as Cinema;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edit cinema data"),
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Edit cinema data"),
+        ),
+        body: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.always,
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              children: [
+                // добавить к каждому сравнение с исходным значением в поле, чтобы не вызывать апи в случае если данные не изменились
+                TextFormField(
+                  onChanged: (String value) => {routesData.cinema.name = value},
+                  decoration: const InputDecoration(labelText: "Cinema name"),
+                  initialValue: routesData.cinema.name,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter cinema name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  onChanged: (String value) =>
+                      {routesData.cinema.cityName = value},
+                  decoration: const InputDecoration(labelText: "City name"),
+                  initialValue: routesData.cinema.cityName,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter city name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  onChanged: (String value) =>
+                      {routesData.cinema.address = value},
+                  decoration: const InputDecoration(labelText: "Address"),
+                  initialValue: routesData.cinema.address,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter address of cinema';
+                    }
+                    return null;
+                  },
+                ),
+                OverflowBar(
+                  alignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        deleteCinema(routesData.cinema.idCinema);
+                        Navigator.pushReplacementNamed(context, '/list_cinemas',
+                            arguments: routesData);
+                      },
+                      child: const Text("DELETE"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // спросить пользователя сохранять ли изменения, если да вызываем функцию:
+                        editCinema(routesData.cinema);
+                        Navigator.pushReplacementNamed(context, '/list_halls',
+                            arguments: routesData);
+                      },
+                      child: const Text("EDIT HALLS->"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        editCinema(routesData.cinema);
+                        Navigator.pushReplacementNamed(context, '/list_cinemas',
+                            arguments: routesData);
+                      },
+                      child: const Text("SAVE"),
+                    ),
+                  ],
+                )
+              ],
+            )),
       ),
-      body: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.always,
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            children: [
-              // добавить к каждому сравнение с исходным значением в поле, чтобы не вызывать апи в случае если данные не изменились
-              TextFormField(
-                onChanged: (String value) => {routesData.cinema.name = value},
-                decoration: const InputDecoration(labelText: "Cinema name"),
-                initialValue: routesData.cinema.name,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Enter cinema name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                onChanged: (String value) => {routesData.cinema.cityName = value},
-                decoration: const InputDecoration(labelText: "City name"),
-                initialValue: routesData.cinema.cityName,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Enter city name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                onChanged: (String value) => {routesData.cinema.address = value},
-                decoration: const InputDecoration(labelText: "Address"),
-                initialValue: routesData.cinema.address,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Enter address of cinema';
-                  }
-                  return null;
-                },
-              ),
-              OverflowBar(
-                alignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      deleteCinema(routesData.cinema.idCinema);
-                      Navigator.pushReplacementNamed(context, '/list_cinemas',
-                          arguments: routesData);
-                    },
-                    child: const Text("DELETE"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // спросить пользователя сохранять ли изменения, если да вызываем функцию:
-                      editCinema(routesData.cinema);
-                      Navigator.pushReplacementNamed(context, '/list_halls',
-                          arguments: routesData);
-                    },
-                    child: const Text("EDIT HALLS->"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      editCinema(routesData.cinema);
-                      Navigator.pushReplacementNamed(context, '/list_cinemas',
-                          arguments: routesData);
-                    },
-                    child: const Text("SAVE"),
-                  ),
-                ],
-              )
-            ],
-          )),
+      onWillPop: () async {
+        Navigator.pushReplacementNamed(context, '/list_cinemas',
+            arguments: routesData);
+        return Future.value(true);
+      },
     );
   }
 }
